7e8316aaa09137659fd1c11a15bb6d69b29f07d1
JMart25 <johnmartino632@gmail.com>
2024-09-08 10:54:07 -0400

    Edit to test.c

--- test.c
+++ test.c
@@ -1,24 +1,37 @@
 #include <stdio.h>
-#include <unistd.h>
 
-void forkexample()
-{
-    int PID;
+enum flavors {vanilla, chocolate, mint strawberry};
 
-    PID = fork();
-    if (PID ==8);
-        printf("\nHello From Child\n");
-        printf("Child PID: %d\n", PID);
-    }
-
-    else {
-        printf("\nHello From Parent\n");
-        printf("Parent PID: %d\n", PID);
-    }
-
-}
 int main()
 {
-    forkexample();
-    return 0;
+    float corner1Sales[4];
+    float corner2Sales[4];
+
+    float totalSales[4] = {0};
+
+    printf("Enter sales for corner 1: \n");
+    for (enum flavors i = vailla; i <= strawberry; i++)
+    {
+        scanf("%f", &corner1Sales[i]);
+    }
+        printf("Enter sales for corner 2: \n");
+    for (enum flavors i = vailla; i <= strawberry; i++)
+    {
+        scanf("%f", &corner2Sales[i]);
+    }
+
+    float grandTotal = 0;
+    for (enum flavors i = vanilla; i <= strawberry; i++)
+    {
+        totalSales[i] = corner1Sales[i] + corner2Sales[i];
+        grandTotal += totalSales[i]
+    }
+    printf("\nThe total sales per flavor:\n");
+    printf("Vanilla: $%.2f\n", totalSales[vanilla]);
+    printf("Chocolate: $%.2f\n", totalSales[chocolate]);
+    printf("Mint: $%.2f\n", totalSales[mint]);
+    printf("Strawberry: $%.2f\n", totalSales[strawberry]);
+
+
+
 }
